9fe7595aa26a4365c69cc3d164d1aaf26f2d26c8
kynoxk0703 <minalopps@gmail.com>
2022-02-20 14:09:52 +0400

    initial commit

---
+++ src/components/Labels.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
+
+export default function Labels() {
+  const { labels, updateLabel } = useContext(GlobalContext);
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
